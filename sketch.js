64617dc5eb9750675b557b87d045697b5b829103
chilli12345 <73940796+chilli12345@users.noreply.github.com>
2020-11-23 17:12:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground,sideBody
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -20,14 +20,14 @@ function setup() {
 	side1.shapeColor="red";
 	side2=createSprite(300, height-90,20,100);
 	side2.shapeColor="red";
-	side2=createSprite(500, height-90,20,100);
-	side2.shapeColor="red";
+	side3=createSprite(500, height-90,20,100);
+	side3.shapeColor="red";
 	
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(200, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite=createSprite(200, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
@@ -38,12 +38,14 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	packageBody = Bodies.circle(400 , 200 , 5 , {restitution:0.4, isStatic:true,});
 	World.add(world, packageBody);
-	
 
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	sideBody = Bodies.rectangle(width/2,660,200,20,{isStatic:true});
+	World.add(world, sideBody);
+	
+   //Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10  );
  	World.add(world, ground);
 
 
@@ -53,18 +55,38 @@ function setup() {
 
 
 function draw() {
+  
   rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  keyPressed();
+  packageSprite.visible=false;
+  if(keyCode === RIGHT_ARROW){
+	  helicopterSprite.velocityX=5;
+  }
+  if(keyWentDown("left")){
+    helicopterSprite.velocityX=-5;
+}
+   keyPressed();
+   if(packageSprite.isTouching(side1)){
+	   side1.visible=false;
+	   side2.visible=false;
+	   side3.visible=false;
+	   packageSprite.visible=false;
+	   helicopterSprite.visible=false;
+	   textSize(30);
+	   fill("red");
+	   text("You Delivered The Package!!",200,200);
+   }
+  
   drawSprites();
+  
   
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-
-  Matter.Body.setStatic(packageBody,false);  
+    packageSprite.visible=true;
+	Matter.Body.setStatic(packageBody,false);  
   }
 }
